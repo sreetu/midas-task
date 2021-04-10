2f06cc97d7260a68f086e91ff9e8ba06d9cfd98a
sreetu <reetusingh24022000@gmail.com>
2021-04-10 12:14:05 +0530

    live

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn midas_task.wsgi
---
+++ midas_task/app/views.py
@@ -0,0 +1,91 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from app.forms import UserForm,UserProfileInfoForm
+
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def index(request):
+    return render(request,'app/index.html')
+
+@login_required
+def special(request):
+    return HttpResponse("logged in currently")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+def register(request):
+
+    registered = False
+
+    if request.method == 'POST':
+
+        # Get info from "both" forms
+        # It appears as one form to the user on the .html page
+        user_form = UserForm(data=request.POST)
+
+        # Check to see both forms are valid
+        if user_form.is_valid():
+
+            # Save User Form to Database
+            user = user_form.save()
+
+            # Hash the password
+            user.set_password(user.password)
+
+            # Update with Hashed password
+            user.save()
+
+            # Registration Successful!
+            registered = True
+
+        else:
+            # One of the forms was invalid if this else gets called.
+            print(user_form.errors)
+
+    else:
+        # Was not an HTTP post so we just render the forms as blank.
+        user_form = UserForm()
+
+    # This is the render and context dictionary to feedback to the signup.html file page.
+    return render(request,'app/signup.html',
+                          {'user_form':user_form,
+                           'registered':registered})
+
+def user_login(request):
+
+    if request.method == 'POST':
+        # First get the username and password supplied
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Django's built-in authentication function:
+        user = authenticate(username=username, password=password)
+
+        # If we have a user
+        if user:
+            #Check if the account is active
+            if user.is_active:
+                # Log the user in.
+                login(request,user)
+                # Send the user back to homepage.
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                # If account is not active:
+                return HttpResponse("Your account is not active.")
+        else:
+            print("Someone tried to login and failed.")
+            print("They used username: {} and password: {}".format(username,password))
+            return HttpResponse("Invalid login details supplied.")
+
+    else:
+        #Nothing has been provided for username or password.
+        return render(request, 'app/login.html', {})
